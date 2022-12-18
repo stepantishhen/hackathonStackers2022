7d396f0dd69f83edca21225311302072122cfb2b
gercovv <sasamrfn@gmail.com>
2022-12-18 10:04:50 +0300

    fix: cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,11 @@ import { TransformInterceptor } from './shared/lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,POST',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = app.get(ConfigService);
