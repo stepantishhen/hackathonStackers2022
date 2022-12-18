482f191b973019f74d86ac9b509e90bc5022cc60
gercovv <sasamrfn@gmail.com>
2022-12-18 18:06:41 +0300

    feat: user data in token

--- server/src/auth/dto/user.ts
+++ server/src/auth/dto/user.ts
@@ -2,8 +2,16 @@ import { User } from '@prisma/client';
 
 export class UserDTO {
   id: string;
+  email: string;
+  firstName: string;
+  surname: string;
+  patronymic: string;
 
   constructor(user: User) {
     this.id = user.id;
+    this.email = user.email;
+    this.firstName = user.firstName;
+    this.surname = user.surname;
+    this.patronymic = user.patronymic;
   }
 }
